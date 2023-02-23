5e892bf7a83aa5ef0c40ada22035fafd8f736f6a
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-02-22 22:02:34 -0500

    removed some comments

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -72,15 +72,6 @@ function handleSearchClick(e){
     document.getElementById("temp").innerHTML = `Temp:  ${data.list[0].main.temp} deg F`;
     document.getElementById("wind").innerHTML = `Wind:  ${data.list[0].wind.speed} MPH`;
     document.getElementById("humidity").innerHTML = `Humidity:  ${data.list[0].main.humidity} %`;
-    // let date = (data.list[0].dt_txt).split(" ")[0];
-    // let dateEl = document.getElementById("city");
-    // dateEl.textContent = `${city}   ${date}`;
-    // let tempEl = document.getElementById("temp");
-    // tempEl.textContent = `Temp:  ${data.list[0].main.temp} deg F`;
-    // let windEl = document.getElementById("wind");
-    // windEl.textContent = `Wind:  ${data.list[0].wind.speed} MPH`;
-    // let humidityEl = document.getElementById("humidity");
-    // humidityEl.textContent = `Humidity:  ${data.list[0].main.humidity} %`;
  }
  
  function fiveDayForecast(data){
@@ -93,23 +84,6 @@ function handleSearchClick(e){
     document.getElementById("dayOne").classList.add('bg-dark');
     document.getElementById("dayOne").classList.add('text-light');
 
-    // let dateDayOneEl = document.getElementById("dateDayOne");
-    // dateDayOneEl.textContent = `${data.list[7].dt_txt.split(" ")[0]}`;
-    // let iconDayOneEl = document.getElementById("iconDayOne")
-    // let iconUrl = `https://openweathermap.org/img/w/${data.list[7].weather[0].icon}.png`;
-    // iconDayOneEl.setAttribute ('src', iconUrl);
-    // let iconDescription = data.list[7].weather[0].description;
-    // iconDayOne.setAttribute ('alt', iconDescription);
-    // let tempDayOneEl = document.getElementById("tempDayOne");
-    // tempDayOneEl.textContent = `Temp:  ${data.list[7].main.temp} deg F`;
-    // let windDayOneEl = document.getElementById("windDayOne");
-    // windDayOneEl.textContent = `Wind:  ${data.list[7].wind.speed} MPH`;
-    // let humidityDayOneEl = document.getElementById("humidityDayOne");
-    // humidityDayOneEl.textContent = `Humidity:  ${data.list[7].main.humidity} %`;
-    // let dayOneEl = document.getElementById("dayOne");
-    // dayOneEl.classList.add("bg-dark");
-    // dayOneEl.classList.add("text-light");
-
     document.getElementById("dateDayTwo").innerHTML = `${data.list[15].dt_txt.split(" ")[0]}`;
     document.getElementById("iconDayTwo").src = `https://openweathermap.org/img/w/${data.list[15].weather[0].icon}.png`;
     document.getElementById("iconDayTwo").alt = `${data.list[15].weather[0].description}`;
@@ -118,23 +92,6 @@ function handleSearchClick(e){
     document.getElementById("humidityDayTwo").innerHTML = `Humidity:  ${data.list[15].main.humidity} %`;
     document.getElementById("dayTwo").classList.add('bg-dark');
     document.getElementById("dayTwo").classList.add('text-light');
-
-    // let dateDayTwoEl = document.getElementById("dateDayTwo");
-    // dateDayTwoEl.textContent = `${data.list[15].dt_txt.split(" ")[0]}`;
-    // let iconDayTwoEl = document.getElementById("iconDayTwo");
-    // let iconTwoUrl = `https://openweathermap.org/img/w/${data.list[15].weather[0].icon}.png`;
-    // iconDayTwoEl.setAttribute ('src', iconTwoUrl);
-    // let iconTwoDescription = data.list[15].weather[0].description;
-    // iconDayOne.setAttribute ('alt', iconTwoDescription);
-    // let tempDayTwoEl = document.getElementById("tempDayTwo");
-    // tempDayTwoEl.textContent = `Temp:  ${data.list[15].main.temp} deg F`;
-    // let windDayTwoEl = document.getElementById("windDayTwo");
-    // windDayTwoEl.textContent = `Wind:  ${data.list[15].wind.speed} MPH`;
-    // let humidityDayTwoEl = document.getElementById("humidityDayTwo");
-    // humidityDayTwoEl.textContent = `Humidity:  ${data.list[15].main.humidity} %`;
-    // let dayTwoEl = document.getElementById("dayTwo");
-    // dayTwoEl.classList.add('bg-dark');
-    // dayTwoEl.classList.add("text-light");
 
     document.getElementById("dateDayThree").innerHTML = `${data.list[23].dt_txt.split(" ")[0]}`;
     document.getElementById("iconDayThree").src = `https://openweathermap.org/img/w/${data.list[23].weather[0].icon}.png`;
@@ -145,22 +102,6 @@ function handleSearchClick(e){
     document.getElementById("dayThree").classList.add('bg-dark');
     document.getElementById("dayThree").classList.add('text-light');
 
-    // let dateDayThreeEl = document.getElementById("dateDayThree");
-    // dateDayThreeEl.textContent = `${data.list[23].dt_txt.split(" ")[0]}`;
-    // let iconDayThreeEl = document.getElementById("iconDayThree");
-    // let iconThreeUrl = `https://openweathermap.org/img/w/${data.list[23].weather[0].icon}.png`;
-    // iconDayThreeEl.setAttribute ('src', iconThreeUrl);
-    // let iconThreeDescription = data.list[23].weather[0].description;
-    // iconDayOne.setAttribute ('alt', iconThreeDescription);
-    // let tempDayThreeEl = document.getElementById("tempDayThree");
-    // tempDayThreeEl.textContent = `Temp:  ${data.list[23].main.temp} deg F`;
-    // let windDayThreeEl = document.getElementById("windDayThree");
-    // windDayThreeEl.textContent = `Wind:  ${data.list[23].wind.speed} MPH`;
-    // let humidityDayThreeEl = document.getElementById("humidityDayThree");
-    // humidityDayThreeEl.textContent = `Humidity:  ${data.list[23].main.humidity} %`;
-    // let dayThreeEl = document.getElementById("dayThree");
-    // dayThreeEl.classList.add('bg-dark');
-    // dayThreeEl.classList.add("text-light");
 
     document.getElementById("dateDayFour").innerHTML = `${data.list[31].dt_txt.split(" ")[0]}`;
     document.getElementById("iconDayFour").src = `https://openweathermap.org/img/w/${data.list[31].weather[0].icon}.png`;
@@ -171,23 +112,6 @@ function handleSearchClick(e){
     document.getElementById("dayFour").classList.add('bg-dark');
     document.getElementById("dayFour").classList.add('text-light');
 
-    // let dateDayFourEl = document.getElementById("dateDayFour");
-    // dateDayFourEl.textContent = `${data.list[31].dt_txt.split(" ")[0]}`;
-    // let iconDayFourEl = document.getElementById("iconDayFour");
-    // let iconFourUrl = `https://openweathermap.org/img/w/${data.list[15].weather[0].icon}.png`;
-    // iconDayFourEl.setAttribute ('src', iconFourUrl);
-    // let iconFourDescription = data.list[31].weather[0].description;
-    // iconDayOne.setAttribute ('alt', iconFourDescription);
-    // let tempDayFourEl = document.getElementById("tempDayFour");
-    // tempDayFourEl.textContent = `Temp:  ${data.list[31].main.temp} deg F`;
-    // let windDayFourEl = document.getElementById("windDayFour");
-    // windDayFourEl.textContent = `Wind:  ${data.list[31].wind.speed} MPH`;
-    // let humidityDayFourEl = document.getElementById("humidityDayFour");
-    // humidityDayFourEl.textContent = `Humidity:  ${data.list[31].main.humidity} %`;
-    // let dayFourEl = document.getElementById("dayFour");
-    // dayFourEl.classList.add('bg-dark');
-    // dayFourEl.classList.add("text-light");
-
     document.getElementById("dateDayFive").innerHTML = `${data.list[39].dt_txt.split(" ")[0]}`;
     document.getElementById("iconDayFive").src = `https://openweathermap.org/img/w/${data.list[39].weather[0].icon}.png`;
     document.getElementById("iconDayFive").alt = `${data.list[39].weather[0].description}`;
@@ -197,22 +121,6 @@ function handleSearchClick(e){
     document.getElementById("dayFive").classList.add('bg-dark');
     document.getElementById("dayFive").classList.add('text-light');
 
-    // let dateDayFiveEl = document.getElementById("dateDayFive");
-    // dateDayFiveEl.textContent = `${data.list[39].dt_txt.split(" ")[0]}`;
-    // let iconDayFiveEl = document.getElementById("iconDayFive");
-    // let iconFiveUrl = `https://openweathermap.org/img/w/${data.list[39].weather[0].icon}.png`;
-    // iconDayFiveEl.setAttribute ('src', iconFiveUrl);
-    // let iconFiveDescription = data.list[39].weather[0].description;
-    // iconDayOne.setAttribute ('alt', iconFiveDescription);
-    // let tempDayFiveEl = document.getElementById("tempDayFive");
-    // tempDayFiveEl.textContent = `Temp:  ${data.list[39].main.temp} deg F`;
-    // let windDayFiveEl = document.getElementById("windDayFive");
-    // windDayFiveEl.textContent = `Wind:  ${data.list[39].wind.speed} MPH`;
-    // let humidityDayFiveEl = document.getElementById("humidityDayFive");
-    // humidityDayFiveEl.textContent = `Humidity:  ${data.list[39].main.humidity} %`;
-    // let dayFiveEl = document.getElementById("dayFive");
-    // dayFiveEl.classList.add('bg-dark');
-    // dayFiveEl.classList.add("text-light");
  }
 let storedCities = localStorage.getItem("searchHistory")
 if (storedCities){
